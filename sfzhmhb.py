b2e2afaa8767f281fa3dcd560c286f46d3a6fbde
lishan <shan.li3@outlook.com>
2024-02-03 21:14:20 +0800

    2024年2月3日更新代码

--- sfzhmhb.py
+++ sfzhmhb.py
@@ -8,7 +8,7 @@ df_sfz_mb=df_sfz_mb.reindex(['客户身份证号码', '合同号',  '客户姓�
            '2023年3月末', '2023年4月末', '2023年5月末', '2023年6月末', '2023年7月末', '2023年8月末',
            '2023年9月末', '2023年10月末', '2023年11月末', '2023年12月末', '全年贷款平均余额',
            '上年度的贷款利差补贴', '实际获得金额', '贷款品种五级', '额度合同号'])
-i=0  #创建一个计数器，用于记录处理到第几个人了
+
 
 '''
 完成了身份证号码的列表ls_sfz的构造，该列表完成了身份证号码的去重处理，包含所有身份证号码，并且每个身份证号码只出现一次。
@@ -39,38 +39,44 @@ def proc_row(tuple_indivi,df_sfz2):
         if type(cell_value)==str:
             for col in range(tuple_indivi[1]):   #遍历每一列，该行数据为字符串类型。
                     dict_hb.add(df_sfz2.iloc[row,col])
-            ls_hb=list(dict_hb)
-            str_hb=",".join(ls_hb)
-            col_end=tuple_indivi[1]
+            ls_hb=list(dict_hb)   #将转置后的df的每一行（即每个字段）构造的字典转换成列表
+            print(row)
+            str_hb=",".join(ls_hb)   #将列表转换成字符串
+            col_end=tuple_indivi[1]  #用于在df最后一列后面构造新的列
             df_sfz2.iloc[row,col_end]=str_hb #将每行的元素都放入一个集合中，再将集合转换为字符串写入对应的“合并”单元格中。
         else:
             for col in range(tuple_indivi[1]):   #遍历每一列，该行数据为数字类型。
                     sum=sum+df_sfz2.iloc[row,col]
             col_end = tuple_indivi[1]
             df_sfz2.iloc[row, col_end] = sum
-    df_sfz2=df_sfz2.T
-    df_sfz_mb.iloc[i,:]=df_sfz2.loc["合并",:]
+    #df_sfz2=df_sfz2.T
+    #df_sfz_mb.iloc[i,:]=df_sfz2.loc["合并",:]
 
 
 '''
 按身份证号码分离每个人的数据
 '''
+i=1  #创建一个计数器，用于记录处理到第几个人了，也就是下面这个身份证号码循环到第几个人了
 for sfzhm in ls_sfz:   #遍历每一个身份证号码
 #sfzhm="35262719810104301X"
-    tf=df_sfz["客户身份证号码"]==sfzhm   #构造用于数据筛选的series,将符合条件的行全部筛选出来了，并且标记为True
-    df_sfz2=df_sfz.loc[tf==True,:]
-    df_sfz2=df_sfz2.T #将筛选出来的每个人的数据的dataframe进行行列转置
+    tf=df_sfz["客户身份证号码"]==sfzhm   #构造用于数据筛选的series,将符合条件的行全部筛选出来了，并且标记为True,df_sfz["客户身份证号码"]取出的值包含重复值
+    df_sfz2=df_sfz.loc[tf==True,:]  #将相同身份证号码的所有数据全部取出来
+    df_sfz2=df_sfz2.fillna({"额度合同号": "空值"})    #将没有值的单元格使用“空值”填充
+    df_sfz2=df_sfz2.T #将筛选出来的每个人的数据的dataframe进行行列转置，便于数据合并并写入新加列“合并”
     df_sfz2=df_sfz2.reindex(['客户身份证号码', '合同号',  '客户姓名名称', '贷款业务品种', '贷款发放额度', '户籍所在地', '户籍地址',
            '利率浮动值', '是否执行先收后返', '贷款执行利率', '2022年12月末', '2023年1月末', '2023年2月末',
            '2023年3月末', '2023年4月末', '2023年5月末', '2023年6月末', '2023年7月末', '2023年8月末',
            '2023年9月末', '2023年10月末', '2023年11月末', '2023年12月末', '全年贷款平均余额',
            '上年度的贷款利差补贴', '实际获得金额', '贷款品种五级', '额度合同号'])   #重新排列索引，将'客户身份证号码'放在'合同号'前面
     tuple_indivi=df_sfz2.shape  #df的shape方法返回的是一个元组类型数据。
-    df_sfz2["合并"]="NaN"  #添加一个新的列用于存放每行合并后的数据
+    df_sfz2["合并"]="NaN"  #添加一个新的列用于存放每行合并后的数据，先使用NaN填充新的列。
     dict_hb=set()   #定义一个空字典，用于存放每一行的所有数据，之所以使用字典是因为他有去重的功能。到下一行先清空这个字典再装入数据。用于数据类型为字符串的行。
     sum=0  #用于数据类型为数字类型的行进行加总计算。
     proc_row(tuple_indivi, df_sfz2)  #调用函数，处理当前这个人的数据
-    i=i+1
+    #df_sfz2=df_sfz2.T
+    df_sfz_mb=df_sfz_mb.T
+    df_sfz_mb.iloc[:,i]=df_sfz2.loc[:,"合并"]   #将每次调用函数后生成的df_sfz2中“合并”一列的数据填写到df_sfz_mb中。
+    i=i+1 #在这里打一个断点结合变量i，可以用于实现对断点设置精细条件
 
 
 
